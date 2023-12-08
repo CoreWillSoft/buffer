55ba4c1da373942a4200ef5d0514a7fe6f0b80dd
mkhytarmkhoian <mkhytar.mkhoian@gmail.com>
2023-12-08 10:32:11 +0200

    Remove JS implementation

--- webpack.config.d/config.js
+++
@@ -1,6 +0,0 @@
-if (config.devServer != null) {
-    config.devServer.headers = {
-        "Cross-Origin-Opener-Policy": "same-origin",
-        "Cross-Origin-Embedder-Policy": "require-corp"
-    }
-}
