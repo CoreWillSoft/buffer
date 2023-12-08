e0580f2751896f3de5a19eb9d8f66824734055c4
mkhytarmkhoian <mkhytar.mkhoian@gmail.com>
2023-12-08 10:32:11 +0200

    Remove JS implementation

--- src/jsTest/kotlin/com/ditchoom/buffer/RunTest.kt
+++
@@ -1,6 +0,0 @@
-package com.ditchoom.buffer
-
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.promise
-
-actual fun <T> runTest(block: suspend () -> T): dynamic = GlobalScope.promise { block() }
